b77f7dfe9d27590da5f9ab653a1f62bee208f10d
bia-pain-bache <155004885+bia-pain-bache@users.noreply.github.com>
2024-06-23 00:24:05 +0330

    Updated Panel version.

--- _worker.js
+++ _worker.js
@@ -20,7 +20,7 @@ let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://cloudflare-dns.com/dns-query';
 
-let panelVersion = '2.3.5';
+let panelVersion = '2.4.1';
 
 if (!isValidUUID(userID)) {
     throw new Error('uuid is not valid');
