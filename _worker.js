365b54fee3b508ee61ca3abdf685d3d28169dee1
TG@MFJD666 <158172744+Lu6618@users.noreply.github.com>
2024-06-29 04:49:28 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -2275,7 +2275,7 @@ const renderHomePage = async (env, hostName, fragConfigs) => {
                 <i class="fa fa-telegram" style="font-size:36px; margin-right: 10px;"></i>
                 <a class="link" href="https://t.me/MFJD666" target="_blank">TG频道</a>
                 <button id="openModalBtn" class="button">更改密码</button>
-                <button type="button" id="logout" style="background: none; margin: 0; border: none; cursor: pointer; font-size: 16px;">注销
+                <button type="button" id="logout" style="background: none; margin: 0; border: none; cursor: pointer; font-size: 16px;">
                     <i class="fa fa-power-off fa-2x" aria-hidden="true"></i>
                 </button>
             </div>
@@ -2563,7 +2563,7 @@ const renderLoginPage = async () => {
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Login</title>
+    <title>用户登录</title>
     <style>
 
         html, body { height: 100%; margin: 0; }
