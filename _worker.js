3c0ee205c8c92aee89e8606301b1df0d5d8b8442
dvquest <dvquest@live.com>
2024-07-03 02:22:50 +0800

    Update _worker.js
    change uuid

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '0fcfaefa-3fdb-40e0-9584-15a3e6cbdf56';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
