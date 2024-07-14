e015eaabe87748e5d80da859261ec3eef5204d5c
大又大 <55383511+xiazhilu@users.noreply.github.com>
2024-07-14 21:38:08 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '876188a3-4482-4d01-91a8-e21d8fd06999';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
