f6024fa87a17f8f4d42c48cdec4958ea7e148859
ad12211 <155971099+ad12211@users.noreply.github.com>
2024-07-17 17:15:55 +0800

    worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '294cbeb3-51e5-962f-4c1e-70d0663c4e3e';
+let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
