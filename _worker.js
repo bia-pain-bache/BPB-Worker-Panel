e8d24a72d4464ddcdfa6f6d1d8c25821cdae2413
panglaojia <43441204+panglaojia@users.noreply.github.com>
2024-07-20 19:17:56 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '0c25a5cd-17e2-4c6b-b43e-d932095f38f8';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
