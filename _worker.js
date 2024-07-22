61852cd214620ad244594d656416f2c99a0404ab
Yhw7903 <49378616+Yhw7903@users.noreply.github.com>
2024-07-22 16:06:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -200,7 +200,7 @@ export default {
 
                     default:
                         // return new Response('Not found', { status: 404 });
-                        url.hostname = "yh999.dns-dynamic.net";
+                        url.hostname = "www.speedtest.net";
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
