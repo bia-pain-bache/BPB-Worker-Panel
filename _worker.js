47c59f4347e4a9454d9077b816cd8c3eeea9ea7b
xiaobangkiss <83955252+xiaobangkiss@users.noreply.github.com>
2024-07-22 10:01:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '631c2154-199c-487c-8814-00732ea97ec4';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
