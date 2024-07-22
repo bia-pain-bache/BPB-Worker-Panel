3639b9fa334857f29adb3470983cbe4d53f28335
Yhw7903 <49378616+Yhw7903@users.noreply.github.com>
2024-07-22 15:56:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -200,7 +200,7 @@ export default {
 
                     default:
                         // return new Response('Not found', { status: 404 });
-                        url.hostname = "example.com";
+                        url.hostname = "yh999.dns-dynamic.net";
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
