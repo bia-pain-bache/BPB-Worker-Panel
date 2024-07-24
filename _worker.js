cb813d3f44883384c9b25d26f5828218998f123f
fisherYYY <fisher7919811221@gmail.com>
2024-07-24 08:01:12 +0800

    Update _worker.js
    cancer server

--- _worker.js
+++ _worker.js
@@ -198,13 +198,12 @@ export default {
                             }
                         });
 
-                    default:
+                    //default:
                         // return new Response('Not found', { status: 404 });
                         //url.hostname = 'www.speedtest.net';
-	                url.hostname = 'www.baidu.com';
-                        url.protocol = 'https:';
-                        request = new Request(url, request);
-                        return await fetch(request);
+	                //url.protocol = 'https:';
+                        //request = new Request(url, request);
+                        //return await fetch(request);
                 }
             } else {
                 return await vlessOverWSHandler(request);
