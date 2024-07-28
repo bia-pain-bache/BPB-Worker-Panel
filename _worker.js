b774d356c7c410c42e095d0cdc2a264b42048bbb
hdy200 <53055418+hdy200@users.noreply.github.com>
2024-07-28 12:19:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+
+//bb78dec1-df63-439d-9d97-2961e179ea0b
+let userID = 'bb78dec1-df63-439d-9d97-2961e179ea0b';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
