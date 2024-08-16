9b71c3c3007eddd3a815dca022592a0a8508c7c3
ripperX309 <37724442+ripperX309@users.noreply.github.com>
2024-08-16 21:01:22 +0800

    worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '47f7923c-f803-4e57-9c41-d64e365cdec0';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
