e51e782ae5f807f5c8c851207c12dde58d48eaf6
RJamshidfar <121728027+RJamshidfar@users.noreply.github.com>
2024-09-27 08:12:51 +0330

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '7c619670-455d-4450-9222-adc2dd3f2b4c';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
