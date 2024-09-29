706a5f5ba0d97f65f0106deb1a34a0fa5a421e68
mohamadisv <mohamadsalehi.irani@gmail.com>
2024-09-30 01:04:14 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '5fd507fd-b986-4cc1-9786-008a3eb507d7';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
