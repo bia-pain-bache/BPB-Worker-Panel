020f8923a3092f4e6b6cc0abd537a9b59ffb6d9d
bianchangshan <130048115+bianchangshan@users.noreply.github.com>
2024-10-04 10:50:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -287,7 +287,7 @@ export default {
 
                     default:
                         // return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.speedtest.net';
+                        url.hostname = 'ruiwang26.top';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
