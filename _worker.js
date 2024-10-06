e1252d7b9325727db7cb82d1d34acef2a9f03fd6
loismlopez <70675490+loismlopez@users.noreply.github.com>
2024-10-06 09:29:19 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '5cd3f62a-0788-4ee0-9eb6-66450d3c051d';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
