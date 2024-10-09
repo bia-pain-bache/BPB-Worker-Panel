cea4d65166cb1bc6cd106bd1113663723f76f322
zxd-bug <zhaoxidong@gmail.com>
2024-10-09 22:56:32 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -286,7 +286,7 @@ export default {
                         });
 
                     default:
-                        // return new Response('Not found', { status: 404 });
+                        return new Response('Not found', { status: 404 });
                         url.hostname = 'www.speedtest.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
