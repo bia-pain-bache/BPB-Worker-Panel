6ac547b06f2e39cb328a9faf1574e9bcca21d129
mic1user1 <153880708+mic1user1@users.noreply.github.com>
2024-10-23 16:22:03 +0800

    _worker.js
    404

--- _worker.js
+++ _worker.js
@@ -4328,6 +4328,7 @@ var worker_default = {
               }
             });
           default:
+            return new Response('Not found', { status: 404 });
             url.hostname = "www.speedtest.net";
             url.protocol = "https:";
             request = new Request(url, request);
