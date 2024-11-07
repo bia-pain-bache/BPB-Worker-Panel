988acce6ab8118f102efa48d8ea479ef7f9abf31
vpvp1 <vpvp1000.1@gmail.com>
2024-11-07 13:15:31 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4085,7 +4085,7 @@ var SignJWT = class extends ProduceJWT {
 };
 
 // src/worker.js
-var userID = "89b3cbba-e6ac-485a-9481-976a0415eab9";
+var userID = "019c7add-871a-4d85-b79b-a071a06b20be";
 var trojanPassword = `bpb-trojan`;
 var proxyIPs = ["bpb.yousef.isegaro.com"];
 var defaultHttpPorts = ["80", "8080", "2052", "2082", "2086", "2095", "8880"];
