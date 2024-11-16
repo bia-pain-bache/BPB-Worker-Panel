8fa294bf8be07b880638e16b256327ea09ea95a8
bia-pain-bache <forir.freedom@gmail.com>
2024-11-16 21:55:51 +0330

    Updated UI.

--- src/pages/home.js
+++ src/pages/home.js
@@ -426,10 +426,10 @@ export async function renderHomePage (request, env, proxySettings, isPassSet) {
                         <input type="text" id="cleanIPs" name="cleanIPs" value="${cleanIPs.replaceAll(",", " , ")}">
                     </div>
                     <div class="form-control">
-                        <label for="scanner">🔎 IP Scanner</label>
+                        <label for="scanner">🔎 Clean IP Scanner</label>
                         <a href="https://github.com/bia-pain-bache/Cloudflare-Clean-IP-Scanner/releases/tag/v2.2.5" name="scanner" target="_blank" style="width: 100%;">
                             <button type="button" id="scanner" class="button">
-                                Scan now
+                                Download Scanner
                                 <span class="material-symbols-outlined">open_in_new</span>
                             </button>
                         </a>
