c4487e764510c5282c02525b717385f1864ba496
mingming-ctr <mingyongcheng@qq.com>
2024-11-22 15:27:35 +0800

    Update worker.js
    取消代理访问
    url.hostname = 'www.speedtest.net';

--- src/worker.js
+++ src/worker.js
@@ -275,7 +275,7 @@ export default {
                         });
 
                     default:
-                        // return new Response('Not found', { status: 404 });
+                        return new Response('Not found', { status: 404 });
                         url.hostname = 'www.speedtest.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
