b45cb811f64d328039193c27306e2857cbb024f6
alhricke <alhricke@foxmail.com>
2024-11-27 21:42:40 +0800

    Update helpers.js

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -61,7 +61,7 @@ export async function handlePanel(request, env) {
 
 export async function fallback(request) {
     const url = new URL(request.url);
-    url.hostname = 'www.speedtest.net';
+    url.hostname = 'upnp.us.kg';
     url.protocol = 'https:';
     request = new Request(url, request);
     return await fetch(request);
