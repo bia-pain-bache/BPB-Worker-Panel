1a2749c1dc81ad46ce62112b615fe5e9835b0cb9
bia-pain-bache <forir.freedom@gmail.com>
2025-04-26 21:59:36 +0330

    Auto generate UDP noise values.

--- src/assets/error/script.js
+++ src/assets/error/script.js
@@ -1,5 +1,5 @@
 localStorage.getItem('darkMode') === 'enabled' && document.body.classList.add('dark-mode');
-const params = new URLSearchParams(window.location.search);
+const params = new URLSearchParams(window.location.href);
 const errorMessage = params.get('message');
 const errorStack = params.get('stack');
 document.getElementById("error").innerHTML = `⚠️ ${ errorStack || errorMessage }`;
