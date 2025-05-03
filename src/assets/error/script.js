4bf04fcefc0e3c3645d4b87dcb0d6673f65c68b4
xiaomaye <164714070+xiaomaye@users.noreply.github.com>
2025-05-03 19:15:16 +0800

    Delete src directory

--- src/assets/error/script.js
+++
@@ -1,5 +0,0 @@
-localStorage.getItem('darkMode') === 'enabled' && document.body.classList.add('dark-mode');
-const params = new URLSearchParams(window.location.href);
-const errorMessage = params.get('message');
-const errorStack = params.get('stack');
-document.getElementById("error").innerHTML = `⚠️ ${ errorStack || errorMessage }`;
