e66bc3735c3f178a5bc5d37196692b9281729441
bia-pain-bache <155004885+bia-pain-bache@users.noreply.github.com>
2025-06-04 16:15:14 +0330

    Revised WorkerLess DNS, Sanction default DNS.

--- docs/en/docs/configuration/routing-rules.md
+++ docs/en/docs/configuration/routing-rules.md
@@ -60,7 +60,7 @@ There are cases which predefined rules cannot help. For example, if you have blo
 ## Sanction Rules
 
 In case you need some websites to just bypass sanctions and connect directly (without proxy), you can use this section.
-You can set your desired DNS server (which should be a transparent proxy also) and choose preset rules or fill in custom addresses. you can even use configs like `WorkerLess` which do not use any proxy and access sanctioned websites. The default DNS server is Shecan (for iranian users).
+You can set your desired DNS server (which should be a transparent proxy also) and choose preset rules or fill in custom addresses. you can even use configs like `WorkerLess` which do not use any proxy and access sanctioned websites. The default DNS server is [Electro](https://electrotm.org/) (for iranian users).
 
 !!! info
     DNS server can be in shape of an IP (UDP DNS) or DoH like `https://free.shecan.ir/dns-query`.
--- src/cores-configs/xray.js
+++ src/cores-configs/xray.js
@@ -34,8 +34,6 @@ async function buildXrayDNS(outboundAddrs, domainToStaticIPs, isWorkerLess, isWa
         ? [...staticIPs.ipv4, ...staticIPs.ipv6]
         : staticIPs.ipv4;
 
-    if (isWorkerLess) dnsHost["cloudflare-dns.com"] = ["cloudflare.com"];
-
     const hosts = Object.keys(dnsHost).length ? { hosts: dnsHost } : {};
     const isIPv6 = (VLTRenableIPv6 && !isWarp) || (warpEnableIPv6 && isWarp);
     const dnsObject = {
@@ -45,25 +43,28 @@ async function buildXrayDNS(outboundAddrs, domainToStaticIPs, isWorkerLess, isWa
         tag: "dns",
     };
 
-    const finalRemoteDNS = isWorkerLess
-        ? "https://cloudflare-dns.com/dns-query"
-        : isWarp
-            ? "1.1.1.1"
-            : remoteDNS;
+    let skipFallback = true;
+    let finalRemoteDNS = isWarp ? "1.1.1.1" : remoteDNS;
+    
+    if (isWorkerLess) {
+        if (!dnsObject.hosts) dnsObject.hosts = {};
+        dnsObject.hosts["cloudflare-dns.com"] = ["cloudflare.com"];
+        skipFallback = false;
+        dnsObject.disableFallback = true;
+        finalRemoteDNS = "https://cloudflare-dns.com/dns-query";
+        // const fallbackRemoteDns = buildDnsServer('https://dns.google/dns-query', null, null, null, "remote-dns-fallback");
+        // dnsObject.servers.push(fallbackRemoteDns);
+    }
 
     const remoteDnsServer = buildDnsServer(finalRemoteDNS, null, null, null, "remote-dns");
     dnsObject.servers.push(remoteDnsServer);
-    if (isWorkerLess) {
-        const fallbackRemoteDns = buildDnsServer('https://dns.google/dns-query', null, null, null, "remote-dns-fallback");
-        dnsObject.servers.push(fallbackRemoteDns);
-    }
 
     const bypassRules = routingRules.filter(({ type }) => type === 'direct');
 
     outboundAddrs.filter(isDomain).forEach(domain => {
         bypassRules.push({ rule: true, domain: `full:${domain}`, dns: localDNS });
     });
-    
+
     customBypassRules.filter(isDomain).forEach(domain => {
         bypassRules.push({ rule: true, domain: `domain:${domain}`, dns: localDNS });
     });
@@ -83,7 +84,7 @@ async function buildXrayDNS(outboundAddrs, domainToStaticIPs, isWorkerLess, isWa
     const groupedDomainRules = new Map();
     bypassRules.filter(({ rule }) => rule).forEach(({ domain, ip, dns }) => {
         if (ip) {
-            const server = buildDnsServer(dns, [domain], ip ? [ip] : null, true);
+            const server = buildDnsServer(dns, [domain], ip ? [ip] : null, skipFallback);
             dnsObject.servers.push(server);
         } else {
             !groupedDomainRules.has(dns) && groupedDomainRules.set(dns, []);
@@ -94,7 +95,7 @@ async function buildXrayDNS(outboundAddrs, domainToStaticIPs, isWorkerLess, isWa
 
     for (const [dns, domain] of groupedDomainRules) {
         if (domain.length) {
-            const server = buildDnsServer(dns, domain, null, true);
+            const server = buildDnsServer(dns, domain, null, skipFallback);
             dnsObject.servers.push(server);
         }
     }
@@ -145,8 +146,8 @@ function buildXrayRoutingRules(isChain, isBalancer, isWorkerLess, isWarp) {
 
     const finallOutboundTag = isChain ? "chain" : isWorkerLess ? "fragment" : "proxy";
     const outTag = isBalancer ? "all" : finallOutboundTag;
-    const remoteDnsServers = isWorkerLess ? ["remote-dns", "remote-dns-fallback"] : ["remote-dns"];
-    addRoutingRule(remoteDnsServers, null, null, null, null, outTag, isBalancer);
+    // const remoteDnsServers = isWorkerLess ? ["remote-dns", "remote-dns-fallback"] : ["remote-dns"];
+    addRoutingRule(["remote-dns"], null, null, null, null, outTag, isBalancer);
     addRoutingRule(["dns"], null, null, null, null, "direct");
 
     if (bypassLAN) {
@@ -622,7 +623,7 @@ async function buildXrayBestFragmentConfig(hostName, chainProxy, outbound) {
 }
 
 async function buildXrayWorkerLessConfig() {
-    const config = await buildXrayConfig(`💦 BPB F - WorkerLess ⭐`, false, false, false, false, true, true, [], 'dns.google');
+    const config = await buildXrayConfig(`💦 BPB F - WorkerLess ⭐`, false, false, false, false, true, true, [], false);
     return config;
 }
 
