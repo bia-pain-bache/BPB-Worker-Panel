8e428d8699999a948ada544060c2d7a89e6e2c14
bia-pain-bache <155004885+bia-pain-bache@users.noreply.github.com>
2025-08-31 15:12:12 +0330

    Default fingerprint to chrome, Release v3.4.3

--- src/worker.js
+++ src/worker.js
@@ -10,19 +10,20 @@ export default {
 			const upgradeHeader = request.headers.get('Upgrade');
 			init(request, env, upgradeHeader);
 			const path = globalThis.pathName;
-			if (!upgradeHeader || upgradeHeader !== 'websocket') {
+
+			if (upgradeHeader === 'websocket') {
+				if (globalThis.wsProtocol === 'vl') return await VlOverWSHandler(request);
+				if (globalThis.wsProtocol === 'tr') return await TrOverWSHandler(request);
+			} else {
 				if (path.startsWith('/panel')) return await handlePanel(request, env);
 				if (path.startsWith('/sub')) return await handleSubscriptions(request, env);
 				if (path.startsWith('/login')) return await handleLogin(request, env);
 				if (path.startsWith('/logout')) return await logout(request, env);
 				if (path.startsWith('/secrets')) return await renderSecrets();
 				if (path.startsWith('/favicon.ico')) return await serveIcon();
-				return await fallback(request);
-			} else {
-				if (globalThis.wsProtocol === 'vl') return await VlOverWSHandler(request);
-				if (globalThis.wsProtocol === 'tr') return await TrOverWSHandler(request);
-				return await fallback(request);
 			}
+
+			return await fallback(request);
 		} catch (error) {
 			return await handleError(error);
 		}
