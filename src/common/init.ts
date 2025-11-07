d7d56c05a0edcb46f2947631e76a285944f6bb26
bia-pain-bache <155004885+bia-pain-bache@users.noreply.github.com>
2025-11-07 22:02:40 +0330

    Bug fix, closes #1123

--- src/common/init.ts
+++ src/common/init.ts
@@ -113,7 +113,7 @@ export function init(request: Request, env: Env) {
     globalThis.globalConfig = {
         userID: UUID,
         TrPass: TR_PASS,
-        pathName: decodeURI(pathname),
+        pathName: decodeURIComponent(pathname),
         fallbackDomain: FALLBACK || 'speed.cloudflare.com',
         dohURL: DOH_URL || 'https://cloudflare-dns.com/dns-query'
     };
@@ -139,7 +139,7 @@ export function initHttp(request: Request, env: any) {
     const { UUID, TR_PASS, SUB_PATH, kv } = env;
     const { pathname, origin, searchParams, hostname } = new URL(request.url);
 
-    if (!['/secrets', '/favicon.ico'].includes(decodeURI(pathname))) {
+    if (!['/secrets', '/favicon.ico'].includes(decodeURIComponent(pathname))) {
         if (!UUID || !TR_PASS) throw new Error(`Please set ${_VL_CAP_} UUID and ${_TR_CAP_} password first. Visit <a href="${origin}/secrets" target="_blank">here</a> to generate them.`, { cause: "init" });
         if (!isValidUUID(UUID)) throw new Error(`Invalid UUID: ${UUID}`, { cause: "init" });
         if (typeof kv !== 'object') throw new Error(`KV Dataset is not properly set! Please refer to <a href="${_website_}" target="_blank">tutorials</a>.`, { cause: "init" });
